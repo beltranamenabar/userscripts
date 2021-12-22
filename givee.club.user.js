5fa8259c07dae991ad191c41d6a839dbfd6d6b1a
Beltrán Amenábar <beltra.am@gmail.com>
2021-12-22 13:27:44 -0300

    feat: add givee.club userscript

---
+++ givee.club.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Givee Club non-giveaway remover
+// @version      1.0
+// @author       Beltrán Amenábar (https://github.com/beltranamenabar)
+// @description  Removes the giveaways or ads that aren't related to the page in the "All active giveaways" section
+// @homepageURL  https://github.com/beltranamenabar/userscripts
+// @icon         https://givee.club/favicon.ico
+// @updateURL    https://github.com/beltranamenabar/userscripts/raw/master/givee.club.user.js
+// @supportURL   https://github.com/beltranamenabar/userscripts/issues
+// @match        https://givee.club/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Remove ads
+    document.querySelectorAll(".definetelynotanad").forEach(ad => {
+        ad.parentElement.remove()
+    });
+
+    // Remove giveaways that aren't from givee club
+    document.querySelectorAll(".event-source").forEach(nonGiveaway => {
+        nonGiveaway.parentElement.parentElement.parentElement.parentElement.remove();
+    });
+})();
