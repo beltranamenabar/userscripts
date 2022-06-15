7cbbdfdd6b524996b98c35d395537b22852fe3df
Beltrán Amenábar <beltra.am@gmail.com>
2022-06-15 00:32:39 -0400

    feat(givee.club): add support for the new type of card from epic games. Also minor bugfixes

--- givee.club.user.js
+++ givee.club.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Givee Club non-giveaway remover
-// @version      1.1
+// @version      1.2
 // @author       Beltrán Amenábar (https://github.com/beltranamenabar)
 // @description  Removes the giveaways or ads that aren't related to the page in the "All active giveaways" section
 // @homepageURL  https://github.com/beltranamenabar/userscripts
@@ -11,16 +11,27 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Remove ads
-    document.querySelectorAll(".definetelynotanad").forEach(ad => {
-        ad.parentElement.remove()
-    });
-
+    // Check if we are on a giveaway page or not
+    if (window.location.href.match(/https:\/\/givee.club\/[a-z]{2}\/event\/\d+/)) {
+        document.querySelectorAll(".definetelynotanad").forEach(ad => {
+            ad.remove()
+        });
+    }
+    else {
+        document.querySelectorAll(".definetelynotanad").forEach(ad => {
+            ad.parentElement.remove()
+        });
+    }
     // Remove giveaways that aren't from givee club
     document.querySelectorAll(".event-source").forEach(nonGiveaway => {
         nonGiveaway.parentElement.parentElement.parentElement.parentElement.remove();
+    });
+    // They added a new type of giveaway card, so we need to remove it
+    document.querySelectorAll(".event-platform-epicgames").forEach(nonGiveaway => {
+        nonGiveaway.parentElement.parentElement.parentElement.remove();
     });
 })();
