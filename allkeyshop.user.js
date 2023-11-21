72f5879c0dec75152852e8a2dce725b55b6dcb8f
Beltrán Amenábar <beltra.am@gmail.com>
2023-11-21 02:42:09 -0300

    fix(aks): add a setTimeout to the wheel skip function so it rechecks in case the wheel is not found

--- allkeyshop.user.js
+++ allkeyshop.user.js
@@ -1,18 +1,33 @@
 // ==UserScript==
 // @name         AKS Enhancer
-// @version      0.1
+// @version      0.1.1
 // @author       Beltrán Amenábar (https://github.com/beltranamenabar)
 // @description  Simulates having the ASK extension on the page and also skips the animation of the wheel
 // @homepageURL  https://github.com/beltranamenabar/userscripts
 // @icon         https://www.allkeyshop.com/favicon.ico
+// @downloadURL  https://github.com/beltranamenabar/userscripts/raw/main/allkeyshop.user.js
 // @updateURL    https://github.com/beltranamenabar/userscripts/raw/main/allkeyshop.user.js
 // @supportURL   https://github.com/beltranamenabar/userscripts/issues
 // @match        https://www.allkeyshop.com/blog/reward-program/
 // @match        https://cheapdigitaldownload.com/reward-program/
 // @grant        unsafeWindow
-// @run-at       document-idle  
+// @run-at       document-idle
 // ==/UserScript==
 
+/**
+ * Takes the wheel and shortens the animation time, so it's almost instant
+ */
+function skipWheel() {
+    let wheel = unsafeWindow.wheel;
+    if (!wheel || !wheel.context || !wheel.context.renderer || !wheel.context.renderer.spinner) {
+        return false;
+    }
+    let spinner = wheel.context.renderer.spinner;
+    spinner.accelerationDuration = 10;
+    spinner.fullSpeedDuration = 10;
+    spinner.decelerationDuration = 10;
+    return true;
+}
 
 (function() {
     'use strict';
@@ -25,12 +40,11 @@
         }
     }
 
-    // Takes the wheel and shortens the animation time, so it's almost instant
-    let wheel = unsafeWindow.wheel;
-    if (wheel && wheel.context && wheel.context.renderer && wheel.context.renderer.spinner) {
-        let spinner = wheel.context.renderer.spinner;
-        spinner.accelerationDuration = 10;
-        spinner.fullSpeedDuration = 10;
-        spinner.decelerationDuration = 10;
-    }
+    // We use setTimeout to redo the wheel skip in case the wheel is not loaded in document-idle
+    const skipWheelCallback = () => {
+        if (!skipWheel()) {
+            setTimeout(skipWheelCallback, 1000);
+        }
+    };
+    setTimeout(skipWheelCallback);
 })();
